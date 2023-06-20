51f3d7ef6a85492df5b0f255ecb48bd891561853
Isaque Lourenço <isaque.lourenco2@gmail.com>
2023-06-20 13:21:09 -0300

    Página de Manutenção finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import icon from './assets/icon.png'
+import manutencao2 from './assets/pagina-em-manutencao.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        < div className='manutencao' >
+          <img src={manutencao2} className='img-manutencao' alt="Tequaly logo" />
+        </div >
+        <div>
+          <h1>O sistema logo, logo estará de volta...</h1>
+        </div>
+        <img src={icon} className="logo-img" alt="Img logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
