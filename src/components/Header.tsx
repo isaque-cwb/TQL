c9c8553db4dcb1a3696827dccb0a891c1645da9d
isaque-cbw <isaque.lourenco2@gmail.com>
2023-09-17 02:31:16 -0300

    refatorado responsividade e logo registro de apontamento OK

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { useNavigation } from '@react-navigation/native'
 import { IconButton, useTheme, Heading, StyledProps, HStack, Avatar, VStack, Center, Stack, Pressable, Alert, Flex } from 'native-base'
 import { CaretLeft, UserCircle } from 'phosphor-react-native'
 import LogoBranca from '../assets/logoBranca.svg'
-import { Text } from 'react-native'
+import { Image, Text } from 'react-native'
 import { useUser } from '../contexts/auth'
 
 type Props = StyledProps & {
@@ -22,32 +22,25 @@ export function Header({ title, ...rest }: Props) {
 
 
   return (
-    <VStack bg={colors.purple[300]} w={'full'}   >
-      <HStack space={'1'} >
-
-        <Center  >
-          <LogoBranca width={220} />
+    <VStack bg={colors.purple[300]} w={'100%'} px={'1%'} pt={'10%'} >
+      <HStack px={'1%'} alignItems={'center'} justifyContent={'space-between'}>
+        <Center pt={'1'}  >
+          <LogoBranca width={240} />
         </Center>
+        {userData.usr_foto ?
+          <Image source={{ uri: `data:image/jpeg;base64,${userData.usr_foto}` }} width={58} height={58} borderRadius={50} />
+          :
+          <UserCircle size={50} color='#fff' />
+        }
+        <Stack py={'0'} h={50} w={'23%'} pl={'2%'} pt={'0'} >
 
-        <Stack w={'100%'} justifyContent={'center'} mx={0} p={2} pt={3}  >
-          <HStack  >
-            {userData.usr_foto ?
-              <Avatar source={{ uri: `data:image/jpeg;base64,${userData.usr_foto}` }} />
-              :
-              <UserCircle size={50} color='#fff' />
-            }
-            <Stack p={'1.5'} h={50} w={'34%'}  >
-
-              <Text numberOfLines={1} ellipsizeMode="tail"
-                style={{ color: '#fff', }}
-              >Olá </Text >
-              <Text numberOfLines={1} ellipsizeMode="tail"
-                style={{ color: '#fff', overflow: 'hidden', paddingRight: 30 }}
-              >{userData.usr_nome} </Text >
-            </Stack>
-          </HStack>
+          <Text numberOfLines={1} ellipsizeMode="tail"
+            style={{ color: '#fff' }}
+          >Olá </Text >
+          <Text numberOfLines={2} ellipsizeMode="tail"
+            style={{ color: '#fff', overflow: 'hidden', paddingRight: '0%' }}
+          >{userData.usr_nome} </Text >
         </Stack>
-
       </HStack>
       <Pressable
         w={'full'}
